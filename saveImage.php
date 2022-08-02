40e046871b1c729e486250e7f8b8eb5fe0402eaf
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-08-02 21:07:53 +1200

    Updated saveImage.php and Tested a simplified image aspect ratio adjusting code
    updated saveImage.php. Tested and implemented a simplified code in image aspect ratio adjusting code. Was not functioning as intended so also commented it out.

--- saveImage.php
+++ saveImage.php
@@ -58,7 +58,26 @@
             // imagepng($dst,$target_filename_here); // adjust format as needed
             // imagedestroy($dst);
 
-
+            // $maxDim = 800;
+            // $file_name = $_FILES['uploadedImageFile']['tmp_name'];
+            // list($width, $height, $type, $attr) = getimagesize( $file_name );
+            // if ( $width > $maxDim || $height > $maxDim ) {
+            //     $target_filename = $file_name;
+            //     $ratio = $width/$height;
+            //     if( $ratio > 1) {
+            //         $new_width = $maxDim;
+            //         $new_height = $maxDim/$ratio;
+            //     } else {
+            //         $new_width = $maxDim*$ratio;
+            //         $new_height = $maxDim;
+            //     }
+            //     $src = imagecreatefromstring( file_get_contents( $file_name ) );
+            //     $dst = imagecreatetruecolor( $new_width, $new_height );
+            //     imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+            //     imagedestroy( $src );
+            //     imagepng( $dst, $target_filename ); // adjust format as needed
+            //     imagedestroy( $dst );
+            }
 
             // Checks if the uploaded image is valid by checking if the file size is larger than 2,048KiB file size
             // Only an image file less than around 2.5MB/2,048KiB file size will be successfully uploaded in the DB when using the "longblob" datatype
