699e2dbaf649178c5ae8e75e2a1b5cfbe2da4be6
Faaris Khan <tdv2188@autuni.ac.nz>
2022-08-16 21:39:03 +1200

    Updated project to previous state with working database and receipt

--- ajaxRecordForm.js
+++ ajaxRecordForm.js
@@ -1,13 +1,9 @@
 //Creates and uses a function and event called "onreadystatechange" tp indicate when something has finished loading information - used with XHR
 var xhr = createRequest(); 
-function getData(dataSource, divID, nmDesc, nmDateTime, nmPriorityLevel, nmSiteLocation, nmInSiteLocation) { 
+function getData(dataSource, divID, nmDesc, nmDateTime, nmPriorityLevel) { 
     if(xhr) { //Find if XHR is created else no connection or data will be returned
      var obj = document.getElementById(divID); 
-     var requestbody ="&description="+encodeURIComponent(nmDesc)
-     +"&dateTime="+encodeURIComponent(nmDateTime)
-     +"&priority="+encodeURIComponent(nmPriorityLevel)
-     +"&nmSiteLocation="+encodeURIComponent(nmSiteLocation)
-     +"&nmInSiteLocation="+encodeURIComponent(nmInSiteLocation); 
+     var requestbody ="&description="+encodeURIComponent(nmDesc)+"&dateTime="+encodeURIComponent(nmDateTime)+"&priority="+encodeURIComponent(nmPriorityLevel); 
      xhr.open("POST", dataSource, true);  //Used to open link/url or datasource, with the "true" meaning that asynchronous access is requested 
      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); 
      xhr.onreadystatechange = function() 
