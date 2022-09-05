9486b1d725a646de2a57b3f2c62cab7427559120
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-05 14:11:42 +1200

    Created adminlogin form and page

---
+++ adminlogin.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css" rel="stylesheet"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+      <link rel="stylesheet" href="./styles/style.css">
+      <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+      <title>Admin Login</title>
+      <link rel = "icon" type = "image/png" href = "./images/logo.png">
+   </head>
+<body>
+    
+      <!-- Navbar -->
+      <header>
+         <!-- Navbar -->
+         <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+               <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                     <li>
+                        <a href="index.html">
+                           <div class="sub-menu">
+                              <i class="bi bi-house-door"></i>
+                              <p class="menu-title">Home</p>
+                           </div>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="index.html#about-section">
+                           <div class="sub-menu">
+                              <i class="bi bi-info-square"></i>
+                              <p class="menu-title">About</p>
+                           </div>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="index.html#contact-section">
+                           <div class="sub-menu">
+                              <i class="bi bi-chat-left-text"></i>
+                              <p class="menu-title">Contact</p>
+                           </div>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#">
+                           <div class="sub-menu">
+                              <i class="bi bi-person-circle"></i>
+                              <p class="menu-title">Login</p>
+                           </div>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="record.html">
+                           <div class="sub-menu">
+                              <i class="bi bi-pencil-square"></i>
+                              <p class="menu-title">Record</p>
+                           </div>
+                        </a>
+                     </li>
+                  </ol>
+               </nav>
+            </div>
+         </nav>
+         <!-- Navbar -->
+      </header>
+      <!-- Navbar -->
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+        <form action="adminpage.php" method="post">
+            <div class="form-outline">
+            <input type="text" id="form12" class="form-control" name="accesscode"/>
+            <label class="form-label" for="form12">Enter Access Code</label>
+            </div>
+            <input type="submit" id = "submit" value="Submit" name = "submit">
+        </form>
+</body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"></script>
+</html>
