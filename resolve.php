925129881a8d00a63186a8272a4029eb3075864f
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-07 13:33:03 +1200

    Implemented resolve functionality into mysql database
    -Program now supports resolving nearmiss cases in the admin page

--- resolve.php
+++ resolve.php
@@ -1,8 +1,17 @@
 <?php
-    $establishCon = @mysqli_connect("cmslamp14","nearmiss", "cHz4n3armiss2022", "nearmiss");
     if(!$_GET['id']) {
         echo "error";
     } else {
         $id = $_GET['id'];
+        $establishCon = @mysqli_connect("cmslamp14","nearmiss", "cHz4n3armiss2022", "nearmiss");
+
+        $updateQuery = "UPDATE `nearMissFormData` SET `caseStatus` = 'Resolved' WHERE `nearMissID` = '$id'";
+        $updateData = mysqli_query($establishCon, $updateQuery);
+        
+        if($updateData) {
+            header("location:adminpage.php"); 
+        } else {
+            echo "error";
+        }
     }
 ?>
