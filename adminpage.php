4b26bd309ba918da1f34eb5fee976bf35bc016c3
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-24 01:14:50 +1200

    fixed formatting and changed active tab

--- adminpage.php
+++ adminpage.php
@@ -46,13 +46,13 @@
                         </a>
                      </li>
                      <li>
-                           <a href="adminlogin.html">
-                              <div class="sub-menu">
-                                 <i class="bi bi-person-circle"></i>
-                                 <p class="menu-title">Admin</p>
-                              </div>
-                           </a>
-                        </li>
+                        <a href="adminlogin.html">
+                           <div class="sub-menu">
+                              <i class="bi bi-person-circle"></i>
+                              <p class="menu-title">Admin</p>
+                           </div>
+                        </a>
+                     </li>
                      <li>
                         <a href="record.html">
                            <div class="sub-menu">
@@ -97,12 +97,10 @@
          ?>
       <!-- Button trigger modal -->
       <!-- Tabs navs -->
-
-      
       <ul class="nav nav-tabs mb-3 justify-content-center" id="ex1" role="tablist">
          <li class="nav-item" role="presentation">
             <a
-               class="nav-link"
+               class="nav-link active"
                id="ex1-tab-1"
                data-mdb-toggle="tab"
                href="#ex1-tabs-1"
@@ -126,7 +124,7 @@
          </li>
          <li class="nav-item" role="presentation">
             <a
-               class="nav-link active"
+               class="nav-link"
                id="ex1-tab-3"
                data-mdb-toggle="tab"
                href="#ex1-tabs-3"
@@ -150,10 +148,10 @@
          
             $displayQueryResolved = "SELECT * FROM `nearMissFormData` WHERE `caseStatus` = 'Resolved'";
             $selectDataResolved = mysqli_query($establishCon, $displayQueryResolved);
-
+         
             $displayQueryAllCases = "SELECT * FROM `nearMissFormData`";
             $selectAllCases = mysqli_query($establishCon, $displayQueryAllCases);
-
+         
          ?>
       <div class="tab-content" id="ex1-content">
          <div
@@ -282,7 +280,7 @@
                echo"</html>";
                // Frees up the memory, after using the result pointer
                mysqli_free_result($selectDataResolved);
-            ?>
+               ?>
          </div>
          <div class="tab-pane fade show active" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
             <?php
@@ -294,17 +292,17 @@
                echo "</div>";
                if(isset($_GET['search_btn'])) {
                   $searchedForValue = $_GET['casesearch'];
-
+               
                   echo "<div class='searched-case-message'>";
                   echo "<p> You Searched For: <strong> $searchedForValue <strong> </p>";
                   echo "</div>";
-
+               
                   $searchQuery = "SELECT * FROM `nearMissFormData` WHERE `nearMissID` = '$searchedForValue'";
                   $searchThroughDescription = "SELECT * FROM `nearMissFormData` WHERE `nmDesc` LIKE '%$searchedForValue%'";
-
+               
                   $displaySearchResults = mysqli_query($establishCon, $searchQuery);
                   $displayDescriptionResults = mysqli_query($establishCon, $searchThroughDescription);
-
+               
                   if(mysqli_num_rows($displaySearchResults) > 0){
                      echo"<html>";
                      echo"<head>";
@@ -330,7 +328,7 @@
                      echo "<tbody>";
                      echo "'<h3 style='text-align:center'>Search Results for Case ID Query</h3>";
                      echo "<br>";
-
+               
                      while ($row = mysqli_fetch_assoc($displaySearchResults)){
                         echo "<tr>";
                         echo "<td>",$row["nearMissID"],"</td>";
@@ -373,7 +371,7 @@
                      echo"</html>";
                      // Frees up the memory, after using the result pointer
                      mysqli_free_result($displaySearchResults);
-
+               
                   }
                   else if(mysqli_num_rows($displayDescriptionResults) > 0){
                      echo"<html>";
@@ -400,7 +398,7 @@
                      echo "<tbody>";
                      echo "'<h3 style='text-align:center'>Search Results for Description Query</h3>";
                      echo "<br>";
-
+               
                      while ($row = mysqli_fetch_assoc($displayDescriptionResults)){
                         echo "<tr>";
                         echo "<td>",$row["nearMissID"],"</td>";
@@ -443,68 +441,68 @@
                      echo"</html>";
                      // Frees up the memory, after using the result pointer
                      mysqli_free_result($displayDescriptionResults);
-
+               
                   }
                   else{
                      echo "'<h3 style='text-align:center'>No Records Matched with your Search Query!</h3>";
                   }
-
+               
                }
-            ?>
+               ?>
             <?php
-                  echo "'<h3 style='text-align:center'>List of All Cases</h3>";
-                  echo "<br>";
-                  echo"<html>";
-                  echo"<head>";
-                  echo"<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' rel='stylesheet'/>";
-                  echo"<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' rel='stylesheet'/>";
-                  echo"<link href='https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css' rel='stylesheet'/>";
-                  echo"</head>";
-                  echo"<body>";
-                  echo "<div class='table-responsive'>";
-                  echo "<table class='table'>";
-                  echo "<thead>";
-                  echo "<tr>";
-                  echo "<th scope='col'>Case ID</th>";
-                  echo "<th scope='col'>Site Location</th>";
-                  echo "<th scope='col'>Insite Location</th>";
-                  echo "<th scope='col'>Description</th>";
-                  echo "<th scope='col'>Date and Time</th>";
-                  echo "<th scope='col'>Status</th>";
-                  echo "</tr>";
-                  echo "</thead>";
-                  echo "<tbody>";
-
+               echo "'<h3 style='text-align:center'>List of All Cases</h3>";
+               echo "<br>";
+               echo"<html>";
+               echo"<head>";
+               echo"<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' rel='stylesheet'/>";
+               echo"<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' rel='stylesheet'/>";
+               echo"<link href='https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css' rel='stylesheet'/>";
+               echo"</head>";
+               echo"<body>";
+               echo "<div class='table-responsive'>";
+               echo "<table class='table'>";
+               echo "<thead>";
+               echo "<tr>";
+               echo "<th scope='col'>Case ID</th>";
+               echo "<th scope='col'>Site Location</th>";
+               echo "<th scope='col'>Insite Location</th>";
+               echo "<th scope='col'>Description</th>";
+               echo "<th scope='col'>Date and Time</th>";
+               echo "<th scope='col'>Status</th>";
+               echo "</tr>";
+               echo "</thead>";
+               echo "<tbody>";
+               
                while ($row = mysqli_fetch_assoc($selectAllCases)){
-                  echo "<tr>";
-                  echo "<td>",$row["nearMissID"],"</td>";
-                  echo "<td>",$row["nmSiteLocation"],"</td>";
-                  echo "<td>",$row["nmInSiteLocation"],"</td>";
-                  echo "<td>",$row["nmDesc"],"</td>";
-                  echo "<td>",$row["nmDateTime"],"</td>";
-                  echo "<div class='modal fade' id='","case",$row["nearMissID"],"' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>";
-                  echo "<div class='modal-dialog'>";
-                  echo "<div class='modal-content'>";
-                  echo "<div class='modal-header'>";
-                  echo "<h5 class='modal-title' id='exampleModalLabel'>","Case ID: ",$row["nearMissID"],"</h5>";
-                  echo "<button type='button' class='btn-close' data-mdb-dismiss='modal' aria-label='Close'></button>";
-                  echo "</div>";
-                  echo "<div class='modal-body'>";
-                  echo '<img height="465px" width="465px" src=data:image;base64,' .$row['imageFiles']. ' />';
-                  echo "</div>";
-                  echo "<div class='modal-footer'>";
-                  echo "<button type='button' class='btn btn-secondary' data-mdb-dismiss='modal'>Close</button>";
-                  echo "</div>";
-                  echo "</div>";
-                  echo "</div>";
-                  echo "</div>";
-                  if($row["caseStatus"] == "Unresolved"){
-                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to resolve this near-miss case?');\" href='resolve.php?id=",$row["nearMissID"],"' class='btn btn-success'>Resolve</a></td>";
-                  }
-                  else{
-                     echo "<td>",$row["caseStatus"],"</td>";
-                  }
-                  echo "</tr>";
+               echo "<tr>";
+               echo "<td>",$row["nearMissID"],"</td>";
+               echo "<td>",$row["nmSiteLocation"],"</td>";
+               echo "<td>",$row["nmInSiteLocation"],"</td>";
+               echo "<td>",$row["nmDesc"],"</td>";
+               echo "<td>",$row["nmDateTime"],"</td>";
+               echo "<div class='modal fade' id='","case",$row["nearMissID"],"' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>";
+               echo "<div class='modal-dialog'>";
+               echo "<div class='modal-content'>";
+               echo "<div class='modal-header'>";
+               echo "<h5 class='modal-title' id='exampleModalLabel'>","Case ID: ",$row["nearMissID"],"</h5>";
+               echo "<button type='button' class='btn-close' data-mdb-dismiss='modal' aria-label='Close'></button>";
+               echo "</div>";
+               echo "<div class='modal-body'>";
+               echo '<img height="465px" width="465px" src=data:image;base64,' .$row['imageFiles']. ' />';
+               echo "</div>";
+               echo "<div class='modal-footer'>";
+               echo "<button type='button' class='btn btn-secondary' data-mdb-dismiss='modal'>Close</button>";
+               echo "</div>";
+               echo "</div>";
+               echo "</div>";
+               echo "</div>";
+               if($row["caseStatus"] == "Unresolved"){
+                  echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to resolve this near-miss case?');\" href='resolve.php?id=",$row["nearMissID"],"' class='btn btn-success'>Resolve</a></td>";
+               }
+               else{
+                  echo "<td>",$row["caseStatus"],"</td>";
+               }
+               echo "</tr>";
                }
                echo "</tbody>";
                echo "</table>";
@@ -515,7 +513,7 @@
                // Frees up the memory, after using the result pointer
                mysqli_free_result($selectAllCases);
                header("location:adminpage.php"); 
-            ?>
+               ?>
          </div>
       </div>
       <?php
