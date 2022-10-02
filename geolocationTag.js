2b3f995f0d0216bfc22a7e6d470a0a688fba7187
Anuk Silva <vwv9667@autuni.ac.nz>
2022-10-02 19:31:02 +1300

    Added Comments to geolocationTag.js
    Added Comments to geolocationTag.js

--- geolocationTag.js
+++ geolocationTag.js
@@ -1,34 +1,38 @@
+//This JavaScript file will handle the fetching of the geolocation of the user when they use button to detect their location
 const findMyArea = () => {
-  const siteLocInputField = document.querySelector(".siteLoc");
+   const siteLocInputField = document.querySelector(".siteLoc");
 
-  const success = (position) => {
-    console.log(position);
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(latitude + " " + longitude);
+   const success = (position) => {
+      console.log(position);
+      //Saves the latitude co-ordinates
+      const latitude = position.coords.latitude;
+      //Saves the longitude co-ordinates
+      const longitude = position.coords.longitude;
+      console.log(latitude + " " + longitude);
 
-    const geoApiUrl =
-      "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en";
+      const geoApiUrl =
+         "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en";
 
-    fetch(geoApiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        document.getElementById("nmRegionSubdiv").value =
-          data.principalSubdivision + " " + data.principalSubdivisionCode;
-        let caseRegion =
-          data.principalSubdivision + " " + data.principalSubdivisionCode;
-        console.log(data);
-        console.log(caseRegion);
-      });
-  };
+      //Makes requests to the geolocation api
+      fetch(geoApiUrl)
+         .then((res) => res.json())
+         .then((data) => {
+           //The principal subdivision and principal subdivision code will be written in into the input field of the record form with an ID of nmRegionSubdiv
+           document.getElementById("nmRegionSubdiv").value =
+             data.principalSubdivision + " " + data.principalSubdivisionCode;
+           let caseRegion =
+             data.principalSubdivision + " " + data.principalSubdivisionCode;
+           console.log(data);
+           console.log(caseRegion);
+         });
+   };
 
-  const error = () => {
-    siteLocInputField.textContent = "Unable to retrieve your location";
-  };
+   //This will throw an error if the user has disabled the location permission of the webpage
+   const error = () => {
+      siteLocInputField.textContent = "Unable to retrieve your location";
+   };
 
-  navigator.geolocation.getCurrentPosition(success, error);
+   navigator.geolocation.getCurrentPosition(success, error);
 };
 
-document
-  .querySelector(".find-geolocation")
-  .addEventListener("click", findMyArea);
+document.querySelector(".find-geolocation").addEventListener("click", findMyArea);
