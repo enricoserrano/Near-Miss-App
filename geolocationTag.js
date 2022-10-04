752b38325c9e2bc0bafe390371d272fdf054b275
Leandro Machado <leomac2014@gmail.com>
2022-10-04 14:36:20 +1300

    Updates to camera and geolocationTag

--- geolocationTag.js
+++ geolocationTag.js
@@ -1,34 +1,35 @@
+//This JavaScript file will handle the fetching of the geolocation of the user when they use button to detect their location
 const findMyArea = () => {
-//   const status = document.querySelector(".status");
   const siteLocInputField = document.querySelector(".siteLoc");
 
   const success = (position) => {
-    console.log(position);
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(latitude + " " + longitude);
+     console.log(position);
+     //Saves the latitude co-ordinates
+     const latitude = position.coords.latitude;
+     //Saves the longitude co-ordinates
+     const longitude = position.coords.longitude;
+     console.log(latitude + " " + longitude);
 
-    const geoApiUrl =
-      "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en";
+     const geoApiUrl =
+        "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en";
 
-    fetch(geoApiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-
-        document.getElementById("nmRegionSubdiv").value =
-          data.principalSubdivision + " " + data.principalSubdivisionCode;
-        let caseRegion =
-          data.principalSubdivision + " " + data.principalSubdivisionCode;
-        // status.textContent = data.city + " " + data.locality;
-        // siteLocInputField.textContent = data.city + " " + data.locality;
-        console.log(data);
-        console.log(caseRegion);
-      });
+     //Makes requests to the geolocation api
+     fetch(geoApiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          //The principal subdivision and principal subdivision code will be written in into the input field of the record form with an ID of nmRegionSubdiv
+          document.getElementById("nmRegionSubdiv").value =
+            data.principalSubdivision + " " + data.principalSubdivisionCode;
+          let caseRegion =
+            data.principalSubdivision + " " + data.principalSubdivisionCode;
+          console.log(data);
+          console.log(caseRegion);
+        });
   };
 
+  //This will throw an error if the user has disabled the location permission of the webpage
   const error = () => {
-    status.textContent = "Unable to retrieve your location";
-    siteLocInputField.textContent = "Unable to retrieve your location";
+     siteLocInputField.textContent = "Unable to retrieve your location";
   };
 
   navigator.geolocation.getCurrentPosition(success, error);
---
+++ uploadImageFunctions.js
@@ -0,0 +1,23 @@
+// These constant and variables are used in the process of displaying an image preview.
+const imageInput = document.querySelector("#uploaded-image");
+var uploadedImage = "";
+
+// This function is invoked when a user uploads an image. This functions uploads the 
+// users uploaded image into an image preview box.
+imageInput.addEventListener("change", function () {
+   const imageReader = new FileReader();
+   imageReader.addEventListener("load", () => {
+      uploadedImage = imageReader.result;
+      document.querySelector("#show-uploaded-image").style.backgroundImage = `url(${uploadedImage})`;
+   });
+
+   imageReader.readAsDataURL(this.files[0]);
+});
+
+// This function is invoked when a user uploads an image in the record form.
+// When invoked the image preview box with the image and title is displayed and now visible
+// after initially being hidden.
+function showImagePreviewBox() {
+   document.getElementById('show-uploaded-image').style.display = "block";
+   document.getElementById('image-preview-title').style.display = "block";
+}
